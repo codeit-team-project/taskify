7067e59970a043cc04b5b8cdcfb7bbe54505898e
HellenHong <hongjw030@naver.com>
2023-12-19 18:08:45 +0900

    Feat(MainNav): 메인 Nav 바 컴포넌트 생성

--- src/components/MainNav/MainNav.tsx
+++ src/components/MainNav/MainNav.tsx
@@ -1,15 +1,26 @@
+/* 메인 내비게이션 컴포넌트
+
+TODO - 나중에 사이드바 컴포넌트와 합칠 때, 사이드바를 가리지 않게 조정할 것.
+- 랜딩 페이지에서 사용하는 내비게이션
+- string형 인자 theme을 인자로 받음
+- theme 인자가 없다면 light 테마가 적용됨
+ */
+
 import Image from 'next/image'
-import styles from './MainNav.module.scss'
 import Link from 'next/link'
+import styles from './MainNav.module.scss'
 
 function MainNav({ theme = 'light' }) {
+  const logoSrc = theme === 'light' ? 'images/logoLight.svg' : 'images/logoDark.svg'
+  const logoTitleSrc = theme === 'light' ? 'images/logoTitleLight.svg' : 'images/logoTitleDark.svg'
+
   return (
     <nav className={styles['nav-container']} data-theme={theme}>
       <div className={styles['logo-container']}>
         <Link href="/">
-          <Image src="images/logo.svg" width={29} height={34} alt="logo icon" />
+          <Image src={logoSrc} width={29} height={34} alt="logo icon" />
           <Image
-            src="images/logoTitle.svg"
+            src={logoTitleSrc}
             className={styles['logo-title']}
             width={80}
             height={22}
