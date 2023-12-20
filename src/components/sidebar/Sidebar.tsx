a1db781205c76e25753d6353ccdff5fcb6f7d11b
ParkSohyunee <sohyunee016@gmail.com>
2023-12-20 10:04:31 +0900

    Design(Sidebar): 태블릿, 모바일 반응형 스타일 적용

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -9,15 +9,15 @@ export default function Sidebar() {
     <section className={styles.container}>
       <div className={styles.logo}>
         <img src="large_logo.svg" alt="로고" />
-        <img src="large_Taskify.svg" alt="로고" />
+        <img src="large_Taskify.svg" className={styles['logo-text']} alt="로고" />
       </div>
       <div className={styles['title-wrapper']}>
         <span className={styles.title}>Dash Boards</span>
         <button className={styles.action}>
-          <img src="add_box.svg" alt="대시보드 추가하기 버튼" />
+          <img src="add_box.svg" alt="대시보드 추가하기 버튼" style={{ width: '100%' }} />
         </button>
       </div>
-      <div>
+      <div style={{ width: '100%', height: '100%' }}>
         {DASH_BOARD_DATA.dashboards.map((board) => (
           <li key={board.id} className={styles.menus}>
             <SidebarItem board={board} />
--- src/components/sidebar/SidebarItem.tsx
+++ src/components/sidebar/SidebarItem.tsx
@@ -3,7 +3,7 @@
  * 대시보드 response types 분리
  * api 연동 후 mock 데이터 삭제
  * Next Image 컴포넌트로 변경
- * 반응형 스타일 적용
+ * ellipse svg 삭제
  */
 
 import styles from './Sidebar.module.scss'
@@ -26,7 +26,11 @@ export default function SidebarItem({ board }: SidebarItemProps) {
     <>
       <EllipseIcon size={8} color={board.color} />
       <button className={styles.menu}>{board.title}</button>
-      {board.createdByMe && <img src="crown_icon.svg" alt="내가 만든 대시보드" />}
+      <span className={styles['icon-wrapper']}>
+        {board.createdByMe && (
+          <img src="crown_icon.svg" className={styles.icon} alt="내가 만든 대시보드" />
+        )}
+      </span>
     </>
   )
 }
