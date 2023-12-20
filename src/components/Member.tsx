eec74a9b88d8f01db4c689f3812df5b72f51e311
ParkSohyunee <sohyunee016@gmail.com>
2023-12-20 21:19:41 +0900

    Feat(Member): member 컴포넌트 구현

---
+++ src/components/Member.tsx
@@ -0,0 +1,18 @@
+import styles from './members/Member.module.scss'
+
+interface MemberProps {
+  nickname: string
+}
+
+export default function Member({ nickname }: MemberProps) {
+  return (
+    <>
+      <div className={styles['name-wrapper']}>
+        {/* 이미지 컴포넌트 구현예정 */}
+        <img src="circle.svg" />
+        <span className={styles.nickname}>{nickname}</span>
+      </div>
+      <button className={styles['member-button']}>삭제</button>
+    </>
+  )
+}
