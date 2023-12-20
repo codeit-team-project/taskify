47196d84aba5afb78b78ebfde76875a4831435a3
HellenHong <hongjw030@naver.com>
2023-12-20 17:12:21 +0900

    Chore: 외부 이미지 경로 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
