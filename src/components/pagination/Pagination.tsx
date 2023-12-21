eec7214a987e243f53591dbf116012c315cb8e2a
ParkSohyunee <sohyunee016@gmail.com>
2023-12-21 10:53:50 +0900

    Feat(Pagination): 처리할 페이지 없을때 버튼 스타일 적용

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -7,29 +7,40 @@ interface PaginationProps {
   count: number
 }
 
+// api 연동 후 삭제 예정
+const TEST_BOARD_ID = 21
+
 export default function Pagination({ handleDashBoardMembers, count }: PaginationProps) {
-  //   const [currentPage, setCurrentPage] = useState(1)
   const [startPage, setStartPage] = useState(1)
   const lastPage = count !== null ? Math.ceil(count / 2) : 0
+
+  const forwardButtonDefaultStyle = startPage === 1 ? styles['forward-default'] : ''
+  const nextButtonDefaultStyle = startPage === lastPage ? styles['next-default'] : ''
 
   const handlePrevPage = () => {
     if (startPage === 1) return
     setStartPage((prev) => prev - 1)
-    handleDashBoardMembers(startPage - 1, 21)
+    handleDashBoardMembers(startPage - 1, TEST_BOARD_ID)
   }
 
   const handleNextPage = () => {
     if (startPage === lastPage) return
     setStartPage((prev) => prev + 1)
-    handleDashBoardMembers(startPage + 1, 21)
+    handleDashBoardMembers(startPage + 1, TEST_BOARD_ID)
   }
 
   return (
     <div>
-      <button onClick={handlePrevPage} className={styles['left-arrow-button']}>
+      <button
+        onClick={handlePrevPage}
+        className={(styles['arrow-button'], styles.forward, forwardButtonDefaultStyle)}
+      >
         <img src="arrow_forward.svg" alt="구성원보기" />
       </button>
-      <button onClick={handleNextPage} className={styles['right-arrow-button']}>
+      <button
+        onClick={handleNextPage}
+        className={(styles['arrow-button'], styles.next, nextButtonDefaultStyle)}
+      >
         <img src="arrow_next.svg" alt="구성원보기" />
       </button>
     </div>
