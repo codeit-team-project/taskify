d582224f87b7e8e5caed8c45760b5e1311e8fbf3
ParkSohyunee <sohyunee016@gmail.com>
2023-12-22 00:50:49 +0900

    Feat(apis): Invitations api, types 추가

--- src/types/invitations.ts
+++ src/types/invitations.ts
@@ -1,3 +1,14 @@
+import { InvitationType } from './invitedDashBoardListType'
+
 export interface InvitationsValue {
   email: string
+}
+
+export interface InvitationsAboutMeType {
+  cursorId: number
+  invitations: InvitationType[]
+}
+
+export interface InvitedAcceptedType {
+  inviteAccepted: boolean
 }
