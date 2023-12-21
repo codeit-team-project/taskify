3a77201e3a5728b4f28ecf7034a2c5821dde3beb
Hayden <hayden.dev.k@gmail.com>
2023-12-21 19:14:52 +0900

    Refactor: uiType 별 컴포넌트 분리 및. ui 추가

--- src/components/invitedDashBoard/InvitedDashBoard.tsx
+++ src/components/invitedDashBoard/InvitedDashBoard.tsx
@@ -8,18 +8,89 @@ import useDebounce from '@/hooks/useDebounce'
 import { getInvitations } from '@/pages/mydashboard'
 import useDidMountEffect from '@/hooks/useDidMountEffect'
 
+type InvitationsStatus =
+  | 'noInvitations'
+  | 'totalInvitations'
+  | 'searchedInvitations'
+  | 'noSearchedInvitations'
+
+interface NoSearchedInvitationsProps {
+  debouncedSearchTitle: string
+}
+
+interface InvitationsProps {
+  invitationList: InvitedDashBoardListType
+}
+
 interface InvitedDashboardProps {
   list: InvitedDashBoardListType
 }
 
+const getInvitationsType = (
+  debouncedSearchTitle: string | null,
+  invitationList: any[] | null,
+): InvitationsStatus => {
+  if (!debouncedSearchTitle) {
+    return invitationList && invitationList.length > 1 ? 'totalInvitations' : 'noInvitations'
+  } else {
+    return invitationList && invitationList.length > 1
+      ? 'searchedInvitations'
+      : 'noSearchedInvitations'
+  }
+}
+
+function NoInvitations() {
+  return (
+    <div className={styles['no-invitation-container']}>
+      <img
+        className={styles['no-invitation-icon']}
+        src="assets/icon-no-invitation.png"
+        width="100px"
+        height="100px"
+      />
+      <p className={styles['no-invitation-text']}>아직 초대받은 대시보드가 없어요</p>
+    </div>
+  )
+}
+
+function NoSearchedInvitations({ debouncedSearchTitle }: NoSearchedInvitationsProps) {
+  return (
+    <tr>
+      <td colSpan={3}>
+        <p className={styles['no-searched-invitation-text']}>
+          검색어 &quot;{debouncedSearchTitle}
+          &quot;에 해당하는 대시보드가 없어요
+        </p>
+      </td>
+    </tr>
+  )
+}
+
+function Invitations({ invitationList }: InvitationsProps) {
+  return invitationList.map((item) => (
+    <tr key={item.id} className={styles['dashboard-item']}>
+      <td className={styles['dashboard-name']}>{item.dashboard.title}</td>
+      <td className={styles['inviter-name']}>{item.invitee.nickname}</td>
+      <td>
+        <div className={styles['button-container']}>
+          <button className={styles['button-accept']}>수락</button>
+          <button className={styles['button-decline']}>거절</button>
+        </div>
+      </td>
+    </tr>
+  ))
+}
+
 export default function InvitedDashBoard({ list }: InvitedDashboardProps) {
-  const [tableData, setTableData] = useState<InvitedDashBoardListType>(list)
-  const [searchTitle, setSearchTitle] = useState('')
+  const [invitationList, setInvitationList] = useState<InvitedDashBoardListType>(list)
+  const [searchTitle, setSearchTitle] = useState('aaaa')
   const debouncedSearchTitle = useDebounce(searchTitle, 1000)
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTitle(event.target.value)
   }
+
+  const uiType = getInvitationsType(debouncedSearchTitle, invitationList)
 
   useDidMountEffect(() => {
     const getInvitationsByTitle = async () => {
@@ -30,7 +101,7 @@ export default function InvitedDashBoard({ list }: InvitedDashboardProps) {
           (item: InvitedDashBoardItemType) => !item.inviteAccepted,
         )
 
-        setTableData(newInvitations)
+        setInvitationList(newInvitations)
       } catch (error) {
         console.error('Error fetching data:', error)
         return {
@@ -46,8 +117,9 @@ export default function InvitedDashBoard({ list }: InvitedDashboardProps) {
   return (
     <section className={styles['container']}>
       <h2 className={styles['title']}>초대받은 대시보드</h2>
-
-      {tableData && debouncedSearchTitle ? (
+      {uiType === 'noInvitations' ? (
+        <NoInvitations />
+      ) : (
         <table className={styles['invited-dashboard-table']}>
           <colgroup>
             <col className={styles['column-first']} />
@@ -82,31 +154,13 @@ export default function InvitedDashBoard({ list }: InvitedDashboardProps) {
             </tr>
           </thead>
           <tbody>
-            {tableData.length > 1 &&
-              tableData?.map((item) => (
-                <tr key={item.id} className={styles['dashboard-item']}>
-                  <td className={styles['dashboard-name']}>{item.dashboard.title}</td>
-                  <td className={styles['inviter-name']}>{item.invitee.nickname}</td>
-                  <td>
-                    <div className={styles['button-container']}>
-                      <button className={styles['button-accept']}>수락</button>
-                      <button className={styles['button-decline']}>거절</button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
+            {uiType === 'noSearchedInvitations' ? (
+              <NoSearchedInvitations debouncedSearchTitle={debouncedSearchTitle} />
+            ) : (
+              <Invitations invitationList={invitationList} />
+            )}
           </tbody>
         </table>
-      ) : (
-        <div className={styles['no-invitation-container']}>
-          <img
-            className={styles['no-invitation-icon']}
-            src="assets/icon-no-invitation.png"
-            width="100px"
-            height="100px"
-          />
-          <p className={styles['no-invitation-text']}>아직 초대받은 대시보드가 없어요</p>
-        </div>
       )}
     </section>
   )
