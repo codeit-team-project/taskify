1b5bbf21a81d2eef928ad8dd80ac7f0241c2e290
HellenHong <hongjw030@naver.com>
2023-12-22 23:30:07 +0900

    Feat: 닉네임 체크 함수 추가

--- src/utils/formInputValidationRules.ts
+++ src/utils/formInputValidationRules.ts
@@ -1,4 +1,4 @@
-import { emailReg, passwordReg } from './regExpressions'
+import { emailReg, nicknameReg, passwordReg } from './regExpressions'
 
 export const emailValidationRules = {
   required: '이메일은 필수 입력입니다.',
@@ -17,5 +17,17 @@ export const passwordValidationRules = {
   pattern: {
     value: passwordReg,
     message: '비밀번호는 영어 대, 소문자와 숫자가 각각 1개 이상 포함되게 해주세요.',
+  },
+}
+
+export const nicknameValidationRules = {
+  required: '닉네임은 필수 입력입니다.',
+  maxLength: {
+    value: 10,
+    message: '열 자 이하로 작성해 주세요.',
+  },
+  pattern: {
+    value: nicknameReg,
+    message: '닉네임에는 공백이나 특수문자가 들어갈 수 없어요.',
   },
 }
