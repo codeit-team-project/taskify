da8fa1ff108fd2226c0ae3e1e4e16e8f848aa9d9
HellenHong <hongjw030@naver.com>
2023-12-22 16:28:17 +0900

    Feat: Layout 컴포넌트 생성

--- src/components/ui/layout/Layout.tsx
+++ src/components/ui/layout/Layout.tsx
@@ -1,10 +1,14 @@
 /* 레이아웃 컴포넌트
 
-1. HomeLayout은 메인 렌딩페이지를 감싸는 레이아웃 컴포넌트
-2. DashboardLayout은 나머지 페이지를 감싸는 레이아웃 컴포넌트
+TODO - 시간 난다면 theme 도 개발해보기.
+- 인자로 children(필수)과 theme(옵션) 을 받습니다.
+- 만약 router.pathname === "/" 이라면 MainNav와 Footer 컴포넌트 렌더링
+- 만약 router.pathname === "/my" 라면 <DashboardNav whatPage="/my" />와 Sidebar 컴포넌트를 렌더링
+- 만약 router.pathname === "/mydashboard" 라면 <DashboardNav whatPage="/mydashboard" />와 Sidebar 컴포넌트를 렌더링
+- 만약 router.pathname === "그 외 다른 path" 라면 <DashboardNav whatPage="" />와 Sidebar 컴포넌트를 렌더링
 */
 
-import { PropsWithChildren } from 'react'
+import { ReactNode } from 'react'
 import { useRouter } from 'next/router'
 import DashboardNav from '@/components/dashboardNav/DashboardNav'
 import Footer from '@/components/footer/Footer'
@@ -12,18 +16,23 @@ import MainNav from '@/components/mainNav/MainNav'
 import Sidebar from '@/components/sidebar/Sidebar'
 import styles from './Layout.module.scss'
 
-export function HomeLayout({ children }: PropsWithChildren) {
-  return (
-    <>
-      <MainNav theme="dark" />
-      {children}
-      <Footer />
-    </>
-  )
+interface LayoutProps {
+  children: ReactNode
+  theme?: 'dark' | 'light'
 }
 
-export function DashboardLayout({ children }: PropsWithChildren) {
+export function Layout({ children, theme = 'dark' }: LayoutProps) {
   const router = useRouter()
+  if (router.pathname === '/') {
+    return (
+      <>
+        <MainNav theme={theme} />
+        {children}
+        <Footer />
+      </>
+    )
+  }
+
   return (
     <>
       <DashboardNav whatPage={router.pathname} />
