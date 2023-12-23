1c34e3a55db961a1e52b57597b6012a1f2d2b870
HellenHong <hongjw030@naver.com>
2023-12-23 20:38:48 +0900

    Feat: user와 토큰을 담는  전역변수 생성

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,27 @@
+import { PropsWithChildren, createContext, useState } from 'react'
+import { LoginType } from '@/types/auth'
+
+const NONE_USER: LoginType = {
+  user: null,
+  accessToken: null,
+}
+
+interface AuthContextProps {
+  auth: LoginType | null
+  setAuth: null | ((s: LoginType) => void)
+}
+
+const INITIAL_AUTH_CONTEXT: AuthContextProps = {
+  auth: null,
+  setAuth: null,
+}
+
+const AuthContext = createContext(INITIAL_AUTH_CONTEXT)
+
+export const AuthProvider = ({ children }: PropsWithChildren) => {
+  const [auth, setAuth] = useState<LoginType>(NONE_USER)
+
+  return <AuthContext.Provider value={{ auth, setAuth }}>{children}</AuthContext.Provider>
+}
+
+export default AuthContext
