fbf273876a83d32b6e4ad5f009c252b5b5c62b9d
HellenHong <hongjw030@naver.com>
2023-12-23 21:15:47 +0900

    Feat: 로그인 된 상태에서 sign페이지 접근 시 mydashboard로 이동

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -3,10 +3,23 @@
 TODO - 나중에 되면 비밀번호 찾기 같은 것도 만들어볼 수 있지 않을까 가능하다면 해볼것.
 */
 
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
 import SignLayout from '@/components/ui/layout/SignLayout'
 import SignupForm from '@/components/signForm/SignupForm'
+import useAuthContext from '@/hooks/useAuth'
 
 export default function SignUpPage() {
+  const { auth } = useAuthContext()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (auth) {
+      router.push('/mydashboard')
+    }
+  }, [auth, router])
+
   return (
     <SignLayout isSignin={false}>
       <SignupForm />
