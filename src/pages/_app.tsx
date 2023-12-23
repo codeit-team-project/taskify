9316e932a1fa70290ba8ea7b80c079790e3213b9
ParkSohyunee <sohyunee016@gmail.com>
2023-12-23 21:09:56 +0900

    Fix(app): react-query QueryClient 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,9 @@ import type { AppProps } from 'next/app'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient()
+const queryClient = new QueryClient()
 
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
