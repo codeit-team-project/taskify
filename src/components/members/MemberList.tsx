c55ed0b3493078c526a940675eca8cfdd9b39433
ParkSohyunee <sohyunee016@gmail.com>
2023-12-23 10:57:03 +0900

    Feat(MemberList): placeholderData 옵션 추가

--- src/components/members/MemberList.tsx
+++ src/components/members/MemberList.tsx
@@ -10,7 +10,7 @@
  * default size 값 확인하기
  */
 
-import { QueryClient, useQuery } from '@tanstack/react-query'
+import { QueryClient, keepPreviousData, useMutation, useQuery } from '@tanstack/react-query'
 import styles from './Member.module.scss'
 import p from '../pagination/Pagination.module.scss'
 
@@ -22,12 +22,16 @@ import { DashBoardMembers } from '@/types/members'
 // import Pagination from '@/components/pagination/Pagination'
 import Member from './Member'
 import { useEffect, useState } from 'react'
+import { createDashBoardInvitations } from '@/api/dashboards/createDashboardsInvitations'
+import { getDashBoardInvitations } from '@/api/dashboards/getDashboardsInvitations'
+import { InvitationsType } from '@/types/invitedDashBoardListType'
+import { putInvitations } from '@/api/invitations/putInvitations'
 
 interface MomberListProps {
   dashBoardId: number
 }
 
-export default function MemberList({ dashBoardId = 116 }: MomberListProps) {
+export default function MemberList({ dashBoardId = 119 }: MomberListProps) {
   const [currentPage, setCurrentPage] = useState(1)
 
   const queryClient = new QueryClient()
@@ -35,6 +39,8 @@ export default function MemberList({ dashBoardId = 116 }: MomberListProps) {
   const { data, isPlaceholderData } = useQuery<DashBoardMembers>({
     queryKey: ['dashBoardMembers', dashBoardId, currentPage],
     queryFn: () => getDashBoardMembers(dashBoardId, currentPage),
+    placeholderData: keepPreviousData,
+    staleTime: 3000,
   })
 
   console.log(data)
@@ -43,7 +49,7 @@ export default function MemberList({ dashBoardId = 116 }: MomberListProps) {
   //******************************** */
 
   // const [startPage, setStartPage] = useState(1)
-  const lastPage = data?.totalCount !== undefined ? Math.ceil(data?.totalCount / 2) : 0
+  const lastPage = data?.totalCount !== undefined ? Math.ceil(data?.totalCount / 2) : ''
 
   const forwardButtonDefaultStyle = currentPage === 1 ? styles['forward-default'] : ''
   const nextButtonDefaultStyle = currentPage === lastPage ? styles['next-default'] : ''
@@ -62,12 +68,10 @@ export default function MemberList({ dashBoardId = 116 }: MomberListProps) {
 
   useEffect(() => {
     // 만약에 호출해야할 데이터가 있다면 prefetch를 해줘
-    if (!isPlaceholderData) {
-      queryClient.prefetchQuery({
-        queryKey: ['dashBoardMembers', currentPage + 1],
-        queryFn: () => getDashBoardMembers(dashBoardId, currentPage + 1),
-      })
-    }
+    queryClient.prefetchQuery({
+      queryKey: ['dashBoardMembers', currentPage + 1],
+      queryFn: () => getDashBoardMembers(dashBoardId, currentPage + 1),
+    })
   }, [currentPage, dashBoardId, isPlaceholderData, queryClient])
 
   return (
@@ -75,7 +79,9 @@ export default function MemberList({ dashBoardId = 116 }: MomberListProps) {
       <div className={styles['card-info']}>
         <span className={styles['card-title']}>구성원</span>
         <div className={styles['card-action']}>
-          <span className={styles.pages}>1 페이지 중 1</span>
+          <span className={styles.pages}>
+            {lastPage} 페이지 중 {currentPage}
+          </span>
           {/* <Pagination count={data ? data?.totalCount : 0} /> */}
           <div>
             <button
