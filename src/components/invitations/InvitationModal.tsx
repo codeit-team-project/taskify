10262916a79e299fdba7e280906873582fee55df
ParkSohyunee <sohyunee016@gmail.com>
2023-12-25 19:26:38 +0900

    Feat(InvitationModal): isPending, input 값 없을때 버튼 비활성화 및 관련 스타일 적용

--- src/components/invitations/InvitationModal.tsx
+++ src/components/invitations/InvitationModal.tsx
@@ -1,11 +1,11 @@
 /**
  * @TODO 초대하기 모달
  * ㄴ 초대하기 버튼 누르면 초대하기 모달이 나타남 (완료)
- * ㄴ input으로 이메일을 입력하면 초대 버튼 활성화, 미입력시 비활성화, 최소 기능
- * ㄴ 버튼을 클릭했을 때 data 유효성 검사 (4가지 예외 상황)
- * ㄴ 예외 상황에 따른 에러 message 스타일링 (칼럼 모달 디자인 참고)
+ * ㄴ input으로 이메일을 입력하면 초대 버튼 활성화, 미입력시 비활성화, 최소 기능 (완료)
+ * ㄴ 버튼을 클릭했을 때 data 유효성 검사 (4가지 예외 상황) (완료)
+ * ㄴ 예외 상황에 따른 에러 message 스타일링 (칼럼 모달 디자인 참고) (완료)
  * ㄴ 초대하기 버튼스타일링 (+ 빈응형)
- * ㄴ isPending, 에러일때 비활성화 스타일
+ * ㄴ isPending, 에러일때 비활성화 스타일 (완료)
  */
 import { ChangeEvent, useState } from 'react'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
@@ -29,31 +29,27 @@ export default function InvitationModal({ dashBoardId, onClose }: InvitationModa
   const { mutate: createInvitation, isPending } = useMutation({
     mutationKey: ['createInvitation'],
     mutationFn: createDashBoardInvitations,
-    // onSuccess 로직을 분리하는 방법?
-    onSuccess: async () => {
+    // onSuccess 로직을 분리하는 방법
+    onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['dashBoardsInvitations', dashBoardId],
       })
       setErrorMessage('')
-      await onClose()
+      onClose()
     },
     onError: (error) => {
-      console.log(isPending)
-
       if (error instanceof AxiosError) {
         setErrorMessage(error.response?.data.message)
       }
     },
   })
-  console.log(`pending: ${isPending}`)
 
   const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value)
   }
 
   const handleCreateInvitation = async () => {
-    if (!inputValue) {
-      console.log('인풋없음') // 비활성화일때 스타일링
+    if (!inputValue || isPending) {
       return
     }
 
@@ -85,7 +81,9 @@ export default function InvitationModal({ dashBoardId, onClose }: InvitationModa
       <div className={styles.contents}>
         <p className={styles['sub-title']}>이메일</p>
         <input
-          className={classNames(styles.input, { [styles['input-error']]: !!errorMessage })}
+          className={classNames(styles.input, {
+            [styles['input-error']]: !!errorMessage,
+          })}
           onChange={handleChangeInput}
           value={inputValue}
         />
@@ -95,7 +93,12 @@ export default function InvitationModal({ dashBoardId, onClose }: InvitationModa
         <button className={styles['default-button']} onClick={() => onClose()}>
           취소
         </button>
-        <button className={styles['primary-button']} onClick={handleCreateInvitation}>
+        <button
+          className={classNames(styles['primary-button'], {
+            [styles.inActive]: !inputValue || isPending,
+          })}
+          onClick={handleCreateInvitation}
+        >
           초대
         </button>
       </div>
