c7b6e36d949abf3b84161df4ec65718b6daec268
ParkSohyunee <sohyunee016@gmail.com>
2023-12-25 15:14:21 +0900

    Feat: 대시보드 초대 api 에러처리 추가

--- src/api/dashboards/createDashboardsInvitations.ts
+++ src/api/dashboards/createDashboardsInvitations.ts
@@ -1,5 +1,6 @@
 import axiosInstance from '@/commons/lib/axiosInstance'
 import { InvitationsValue } from '@/types/invitations'
+import { AxiosError } from 'axios'
 import { InvitationType } from '@/types/invitedDashBoardListType'
 
 interface CreateInvitationsType {
@@ -8,6 +9,13 @@ interface CreateInvitationsType {
 }
 
 export const createDashBoardInvitations = async ({ id, data }: CreateInvitationsType) => {
-  const response = await axiosInstance.post<InvitationType>(`/dashboards/${id}/invitations`, data)
-  return response.data
+  try {
+    const response = await axiosInstance.post<InvitationType>(`/dashboards/${id}/invitations`, data)
+    return response.data
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      // console.log(error.response?.data) // {message: '이메일 형식이 올바르지 않습니다.'} -> 머지 전 주석 삭제 예정
+      return error.response?.data
+    }
+  }
 }
