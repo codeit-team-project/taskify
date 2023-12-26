bb6b1723a55ad00a6895f68db3a6014442deb664
ParkSohyunee <sohyunee016@gmail.com>
2023-12-26 13:58:21 +0900

    Feat(InvitationList): 초대 내역 페이지네이션 기능 구현

--- src/api/dashboards/getDashboardsInvitations.ts
+++ src/api/dashboards/getDashboardsInvitations.ts
@@ -1,12 +1,15 @@
 import axiosInstance from '@/commons/lib/axiosInstance'
 import { InvitationsType } from '@/types/invitedDashBoardListType'
 
-// page, size를 인자로 받아서 쿼리스트링을 만들 수 있습니다.
-// 쿼리스트링 부분 수정해서 사용해주세요.
+export const getDashBoardInvitations = async (
+  dashBoardId: number,
+  currentPage: number = 1,
+  size: number = 5,
+) => {
+  const QUERY_STRING = `?page=${currentPage}&size=${size}`
 
-export const getDashBoardInvitations = async (dashBoardId: number) => {
   const response = await axiosInstance.get<InvitationsType>(
-    `/dashboards/${dashBoardId}/invitations`,
+    `/dashboards/${dashBoardId}/invitations${QUERY_STRING}`,
   )
   return response.data
 }
