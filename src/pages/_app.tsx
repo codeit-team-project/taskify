1d9a5dd44e3b99621c0ea716b6f32599d8eb12c6
HellenHong <hongjw030@naver.com>
2023-12-26 17:16:05 +0900

    Feat: 토큰 있을 때엔 signin, signup, 메인 페이지 접근 못하게 코드 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,25 @@ import type { AppProps } from 'next/app'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { CookiesProvider } from 'react-cookie'
+import { useRouter } from 'next/router'
+import { getCookie } from '@/utils/cookie'
+import { useEffect } from 'react'
 
 const queryClient = new QueryClient()
 
+const DENIED_FOR_USER = ['/', '/signin', '/signup']
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const savedToken = getCookie('accessToken')
+  useEffect(() => {
+    if (savedToken) {
+      if (DENIED_FOR_USER.includes(router.pathname)) {
+        router.push('/mydashboard')
+      }
+    }
+  }, [router, savedToken])
+
   return (
     <CookiesProvider>
       <QueryClientProvider client={queryClient}>
