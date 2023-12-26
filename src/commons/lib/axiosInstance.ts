cbdcc82926d03ce25c046b03a72c9ffae0215eb0
HellenHong <hongjw030@naver.com>
2023-12-26 16:38:43 +0900

    Refactor: 에러 전파 막는 코드 작성

--- src/commons/lib/axiosInstance.ts
+++ src/commons/lib/axiosInstance.ts
@@ -24,13 +24,15 @@ axiosInstance.interceptors.response.use(
   async (error) => {
     if (typeof window !== 'undefined') {
       switch (error?.response?.status) {
+        // 401 에러 response를 받으면 signin 페이지로 이동
         case 401:
           alert('로그인이 필요합니다!')
           window.location.href = '/signin'
-          break
+          return new Promise(() => {})
+        // 404 에러 response를 받으면 404 페이지로 이동
         case 404:
           window.location.href = '/404'
-          break
+          return new Promise(() => {})
       }
     }
     return error
