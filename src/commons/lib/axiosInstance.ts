52f3b531551286e7062696033eacdc66987ff1dd
HellenHong <hongjw030@naver.com>
2023-12-26 16:15:02 +0900

    Feat: interceptors 코드 추가

--- src/commons/lib/axiosInstance.ts
+++ src/commons/lib/axiosInstance.ts
@@ -2,12 +2,39 @@ import axios from 'axios'
 import { getCookie } from '@/utils/cookie'
 const TEAM_URL = '1-02'
 
-const accessToken = getCookie('accessToken')
-
 const axiosInstance = axios.create({
   baseURL: `https://sp-taskify-api.vercel.app/${TEAM_URL}`,
 })
 
-axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = getCookie('accessToken')
+    config.headers['Authorization'] = `Bearer ${accessToken}`
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  async (error) => {
+    if (typeof window !== 'undefined') {
+      switch (error?.response?.status) {
+        case 401:
+          alert('로그인이 필요합니다!')
+          window.location.href = '/signin'
+          break
+        case 404:
+          window.location.href = '/404'
+          break
+      }
+    }
+    return error
+  },
+)
 
 export default axiosInstance
