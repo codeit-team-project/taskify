80af256ebbde4226043aba7f1eff04a687ff3a9e
ParkSohyunee <sohyunee016@gmail.com>
2023-12-27 16:27:26 +0900

    Feat(EditDashboard): 대시보드 수정 기능 구현

--- src/components/dashboard/EditDashboard.tsx
+++ src/components/dashboard/EditDashboard.tsx
@@ -1,7 +1,7 @@
 /**
  * @todo
  * 해당 대시보드 기존 값이 default로 설정되어 있음 (완료)
- * 이름이나 색을 바꾸고 '변경' 버튼을 누르면 대시보드가 수정
+ * 이름이나 색을 바꾸고 '변경' 버튼을 누르면 대시보드가 수정 (완료)
  * 컬러팔레트 svg 파일 말고 css로 작업 (완료)
  * 모바일 사이즈에서 컬러팔레트 선택된 것만 보이기
  * 수정후에 사이드바, 네브바 대시보드 이름 변경 확인하기
@@ -9,26 +9,37 @@
  */
 
 import { ChangeEvent, useEffect, useState } from 'react'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import classNames from 'classnames'
 
 import styles from './EditDashboard.module.scss'
 import ColorPicker from '@/components/colorPicker/ColorPicker'
 
-import { DashBoardType } from '@/types/dashBoardType'
+import { DashBoardType, DashBoardVauleType } from '@/types/dashBoardType'
 import { getDashBoardsDetail } from '@/api/dashboards/getDashboardsDetail'
+import { editDashBoard } from '@/api/dashboards/editDashboards'
 
 interface EditDashboardProps {
   boardId: number
 }
 
 export default function EditDashboard({ boardId }: EditDashboardProps) {
+  const queryClient = useQueryClient()
+
   const [title, setTitle] = useState('')
   const [selectedColor, setSelectedColor] = useState('')
 
   const { data } = useQuery<DashBoardType>({
     queryKey: ['dashBoardsDetail', boardId],
     queryFn: () => getDashBoardsDetail(boardId),
+  })
+
+  const { mutate: updateDashBoard } = useMutation({
+    mutationKey: ['updateDashBoard'],
+    mutationFn: (data: DashBoardVauleType) => editDashBoard(boardId, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['dashBoardsDetail', boardId] })
+    },
   })
 
   const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
@@ -39,14 +50,23 @@ export default function EditDashboard({ boardId }: EditDashboardProps) {
     setSelectedColor(color)
   }
 
+  const handleUpdateDashboard = () => {
+    if (data?.color === selectedColor && data?.title === title) {
+      return
+    }
+
+    updateDashBoard({
+      title,
+      color: selectedColor,
+    })
+  }
+
   useEffect(() => {
     if (data) {
       setSelectedColor(data.color)
+      setTitle(data.title)
     }
   }, [data])
-
-  // console.log(selectedColor)
-  // console.log(title)
 
   return (
     <section className={styles.container}>
@@ -63,6 +83,7 @@ export default function EditDashboard({ boardId }: EditDashboardProps) {
           className={classNames(styles['primary-button'], {
             [styles.inActive]: !title || !selectedColor,
           })}
+          onClick={handleUpdateDashboard}
         >
           변경
         </button>
