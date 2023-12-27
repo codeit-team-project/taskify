9b5cca67666119ac30f832e85150770f3ef9cfe6
HellenHong <hongjw030@naver.com>
2023-12-27 13:59:52 +0900

    Feat: profile 이미지가 null일 땐 임시 이미지 띄우는 코드 추가

--- src/components/dashboardNav/navProfile/NavProfile.tsx
+++ src/components/dashboardNav/navProfile/NavProfile.tsx
@@ -4,12 +4,13 @@
 - 유저의 프로필을 받아 프로필 이미지와 유저 닉네임을 렌더링하는 컴포넌트
 */
 
+import { FocusEventHandler, MouseEventHandler, useCallback, useState, useEffect } from 'react'
 import Image from 'next/image'
-import styles from './NavProfile.module.scss'
-import { FocusEventHandler, MouseEventHandler, useCallback, useEffect } from 'react'
-import { mockupUser } from '../mockup'
-import { getUser } from '@/api/users/getUser'
 import { useMutation } from '@tanstack/react-query'
+
+import { getUser } from '@/api/users/getUser'
+import { UserType } from '@/types/users'
+import styles from './NavProfile.module.scss'
 
 interface NavProfileProps {
   onOpen: MouseEventHandler
@@ -20,31 +21,35 @@ export default function NavProfile({
   onOpen: handleOpenDropdown,
   onClose: handleCloseDropdown,
 }: NavProfileProps) {
-  /*
-  BUG - 아직 토큰 붙여서 req 보내는 코드가 없어서 이 부분 쓰면 에러남. 
-  후에 코드 머지한다음 이 부분 살리면 됨
-  const [userInfo, setUserInfo] = useState<UserType>();
+  const [userInfo, setUserInfo] = useState<UserType>()
+
   const { mutate } = useMutation({
     mutationKey: ['get-user-key'],
     mutationFn: useCallback(() => getUser(), []),
     onSuccess: (response) => {
-      console.log(response)
-      return response
+      setUserInfo(() => {
+        return { ...response }
+      })
     },
-    onError: () => {},
   })
+
   useEffect(() => {
     mutate()
   }, [mutate])
-  */
+
   return (
     <button
       className={styles['profile-section']}
       onClick={handleOpenDropdown}
       onBlur={handleCloseDropdown}
     >
-      <Image src={mockupUser.user.profileImageUrl} alt="profile img" width={36} height={36} />
-      <div className={styles['nickname']}>{mockupUser.user.nickname}</div>
+      <Image
+        src={userInfo?.profileImageUrl || '/assets/no-profile.png'}
+        alt="profile img"
+        width={36}
+        height={36}
+      />
+      <div className={styles['nickname']}>{userInfo?.nickname}</div>
     </button>
   )
 }
