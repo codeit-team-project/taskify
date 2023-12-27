a39d6c78725ed0e8455e376e1c7100e3226e1b3e
ParkSohyunee <sohyunee016@gmail.com>
2023-12-27 15:54:54 +0900

    Feat(EditDashboard): 대시보드 상세 불러오기

--- src/components/dashboard/EditDashboard.tsx
+++ src/components/dashboard/EditDashboard.tsx
@@ -1,24 +1,35 @@
 /**
  * @todo
- * 해당 대시보드 기존 값이 default로 설정되어 있음
+ * 해당 대시보드 기존 값이 default로 설정되어 있음 (완료)
  * 이름이나 색을 바꾸고 '변경' 버튼을 누르면 대시보드가 수정
- * 컬러팔레트 svg 파일 말고 css로 작업
+ * 컬러팔레트 svg 파일 말고 css로 작업 (완료)
  * 모바일 사이즈에서 컬러팔레트 선택된 것만 보이기
  * 수정후에 사이드바, 네브바 대시보드 이름 변경 확인하기
  * 에러케이스 확인하기
  */
 
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
 import classNames from 'classnames'
 
 import styles from './EditDashboard.module.scss'
 import ColorPicker from '@/components/colorPicker/ColorPicker'
 
-import { DEFAULT_COLOR } from '@/components/colorPicker/dashBoardColorPalette'
+import { DashBoardType } from '@/types/dashBoardType'
+import { getDashBoardsDetail } from '@/api/dashboards/getDashboardsDetail'
 
-export default function EditDashboard() {
+interface EditDashboardProps {
+  boardId: number
+}
+
+export default function EditDashboard({ boardId }: EditDashboardProps) {
   const [title, setTitle] = useState('')
-  const [selectedColor, setSelectedColor] = useState(DEFAULT_COLOR) // 기존 설정값 넣어주기
+  const [selectedColor, setSelectedColor] = useState('')
+
+  const { data } = useQuery<DashBoardType>({
+    queryKey: ['dashBoardsDetail', boardId],
+    queryFn: () => getDashBoardsDetail(boardId),
+  })
 
   const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value)
@@ -28,7 +39,14 @@ export default function EditDashboard() {
     setSelectedColor(color)
   }
 
-  console.log(selectedColor)
+  useEffect(() => {
+    if (data) {
+      setSelectedColor(data.color)
+    }
+  }, [data])
+
+  // console.log(selectedColor)
+  // console.log(title)
 
   return (
     <section className={styles.container}>
@@ -38,7 +56,7 @@ export default function EditDashboard() {
       </div>
       <div className={styles.contents}>
         <p className={styles.description}>대시보드 이름</p>
-        <input className={styles.input} onChange={handleChangeInput} value={title} />
+        <input className={styles.input} onChange={handleChangeInput} defaultValue={data?.title} />
       </div>
       <div className={styles.button}>
         <button
