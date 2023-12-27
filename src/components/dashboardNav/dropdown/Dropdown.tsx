57c7f8be7f4751bfb214d744e984d4b38718a911
HellenHong <hongjw030@naver.com>
2023-12-27 15:00:55 +0900

    Feat: 로그아웃 기능 생성

--- src/components/dashboardNav/dropdown/Dropdown.tsx
+++ src/components/dashboardNav/dropdown/Dropdown.tsx
@@ -1,17 +1,27 @@
 /* 드롭다운 컴포넌트 
 
-TODO - 로그아웃 기능은 이전 pr 중 react cookie pr이 머지된 후 해당 코드를 써서 작성할 것
 - MybouardNav, DashboardNav 컴포넌트의 프로필을 누르면 드롭다운 메뉴가 나옴
 */
 
 import Link from 'next/link'
 import styles from './Dropdown.module.scss'
+import { removeCookie } from '@/utils/cookie'
+import { useRouter } from 'next/router'
 
 export default function Dropdown() {
+  const router = useRouter()
+  const handleLogout = () => {
+    removeCookie('accessToken')
+    alert('로그아웃 되었습니다!')
+    router.push('/')
+  }
+
   return (
     <div className={styles['dropmenu-container']}>
       <div className={styles['button-container']}>
-        <button className={styles['menu-button']}>로그아웃</button>
+        <button className={styles['menu-button']} onClick={handleLogout}>
+          로그아웃
+        </button>
       </div>
       <div className={styles['button-container']}>
         <button className={styles['menu-button']}>
