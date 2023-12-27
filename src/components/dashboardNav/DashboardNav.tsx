43d8b2aac2791c2317605cd632f46a1f060d041c
HellenHong <hongjw030@naver.com>
2023-12-27 14:54:28 +0900

    Refactor: memberlist 코드 삭제

--- src/components/dashboardNav/DashboardNav.tsx
+++ src/components/dashboardNav/DashboardNav.tsx
@@ -1,32 +1,42 @@
 /* DashboardNav 바 컴포넌트
 
+TODO - 대시보드 페이지 작업하면서 한꺼번에 해야 할듯 합니다
 - DashboardLayout 컴포넌트를 감싸는 컴포넌트
 - 대시보드 페이지 위에 보이는 DashboardNav 바 컴포넌트 
 - 옵셔널 인자로 dashboard 상세 조회 정보를 받음
 */
 
 import Image from 'next/image'
-import styles from './BoardNav.module.scss'
+
+import useDropdown from '@/hooks/useDropdown'
+import { DashBoardType } from '@/types/dashBoardType'
 import DashboardNavEditor from './dashboardNavEditor/DashboardNavEditor'
 import Dropdown from './dropdown/Dropdown'
-import { mockDashboardInfo, mockDashboardMemberManyList } from './mockup'
-import useDropdown from '@/hooks/useDropdown'
 import NavProfile from './navProfile/NavProfile'
-import { DashBoardType } from '@/types/dashBoardType'
+import styles from './BoardNav.module.scss'
+import { mockDashboardMemberManyList } from './mockup'
 
 interface DashboardNavProps {
-  dashboardInfo?: DashBoardType | null
+  dashboardInfo?: DashBoardType
 }
 
-export default function DashboardNav({ dashboardInfo = null }: DashboardNavProps) {
+export default function DashboardNav({ dashboardInfo }: DashboardNavProps) {
   const [isVisible, handleOpenDropdown, handleCloseDropdown] = useDropdown()
+  // const [memberList, setMemberList] = useState()
+  // const { data } = useQuery<DashBoardMembers>({
+  //   queryKey: ['dashBoardMembers'],
+  //   queryFn: () => getDashBoardMembers(dashboardInfo.id),
+  // })
+  // useEffect(() => {
+  //   console.log(data)
+  // }, [data])
 
   return (
     <nav className={styles['nav-container']}>
       <div className={styles['title-section']}>
         <>
-          {mockDashboardInfo?.title}
-          {mockDashboardInfo?.createdByMe && (
+          {dashboardInfo?.title}
+          {dashboardInfo?.createdByMe && (
             <span>
               <Image src="/assets/crown_icon.svg" alt="owner" width={20} height={20} />
             </span>
@@ -35,8 +45,8 @@ export default function DashboardNav({ dashboardInfo = null }: DashboardNavProps
       </div>
       <div className={styles['nav-info-section']}>
         <DashboardNavEditor
-          isOwner={mockDashboardInfo.createdByMe}
-          boardId={mockDashboardInfo.id}
+          isOwner={dashboardInfo?.createdByMe}
+          boardId={dashboardInfo?.id}
           members={mockDashboardMemberManyList}
         />
         <NavProfile onOpen={handleOpenDropdown} onClose={handleCloseDropdown} />
