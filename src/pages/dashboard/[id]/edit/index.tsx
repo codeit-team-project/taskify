13eb2e3d2734e842ec682d9fde1435ab9b40aa49
ParkSohyunee <sohyunee016@gmail.com>
2023-12-28 21:52:18 +0900

    Design: 대시보드 페이지 레이아웃 수정

--- src/pages/dashboard/[id]/edit/index.tsx
+++ src/pages/dashboard/[id]/edit/index.tsx
@@ -2,14 +2,26 @@ import { useRouter } from 'next/router'
 
 import EditDashboard from '@/components/dashboard/EditDashboard'
 import DashboardLayout from '@/components/ui/layout/DashboardLayout'
+import MemberList from '@/components/members/MemberList'
+import InvitationList from '@/components/invitations/InvitationList'
 
 export default function DashBoardDetailPage() {
   const router = useRouter()
-  const boardId = router.query.id
+  const boardId = Number(router.query.id)
 
   return (
     <DashboardLayout>
-      <EditDashboard boardId={Number(boardId)} />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1.2rem',
+        }}
+      >
+        <EditDashboard boardId={boardId} />
+        <MemberList dashBoardId={boardId} />
+        <InvitationList dashBoardId={boardId} />
+      </div>
     </DashboardLayout>
   )
 }
