6e2b0b4e73f7241c2702be5c173b74933b7f7b65
ParkSohyunee <sohyunee016@gmail.com>
2023-12-30 01:04:29 +0900

    Feat(DashboardList): 대시보드 생성 모달과 연동

--- src/components/dashboard/DashboardList.tsx
+++ src/components/dashboard/DashboardList.tsx
@@ -4,6 +4,7 @@ import { keepPreviousData, useQuery, useQueryClient } from '@tanstack/react-quer
 import styles from './DashboardList.module.scss'
 
 import DashboardItem from './DashboardItem'
+import NewDashboardModal from './NewDashboardModal'
 import Pagination from '@/components/pagination/Pagination'
 
 import { getDashBoardList } from '@/api/dashboards/getDashboards'
@@ -15,6 +16,7 @@ export default function DashboardList() {
 
   const pageSize = 5 // data per page
   const [currentPage, setCurrentPage] = useState(1)
+  const [isOpenModal, setIsOpenModal] = useState(false)
 
   const { data, isPlaceholderData } = useQuery<DashBoardListType>({
     queryKey: ['dashBoards', currentPage, pageSize],
@@ -27,6 +29,10 @@ export default function DashboardList() {
     router.push(`/dashboard/${boardId}`)
   }
   console.log(data) // 삭제예정
+
+  const handleOpenModal = () => {
+    setIsOpenModal(true)
+  }
 
   const hasMorePage = data && currentPage < Math.ceil(data?.totalCount / pageSize)
 
@@ -41,30 +47,33 @@ export default function DashboardList() {
   }, [currentPage, isPlaceholderData, hasMorePage, queryClient])
 
   return (
-    <section className={styles.container}>
-      <div className={styles.boards}>
-        <div className={styles['add-button']}>
-          <span className={styles.title}>새로운 대시보드</span>
-          <img src="/assets/add_fill_primary.svg" className={styles['add-icon']} />
+    <>
+      <section className={styles.container}>
+        <div className={styles.boards}>
+          <div className={styles['add-button']} onClick={handleOpenModal}>
+            <span className={styles.title}>새로운 대시보드</span>
+            <img src="/assets/add_fill_primary.svg" className={styles['add-icon']} />
+          </div>
+          {data?.dashboards.map((dashboard) => (
+            <li key={dashboard.id} className={styles.list} onClick={handleMoveToPage(dashboard.id)}>
+              <DashboardItem
+                title={dashboard.title}
+                color={dashboard.color}
+                createdByMe={dashboard.createdByMe}
+              />
+            </li>
+          ))}
         </div>
-        {data?.dashboards.map((dashboard) => (
-          <li key={dashboard.id} className={styles.list} onClick={handleMoveToPage(dashboard.id)}>
-            <DashboardItem
-              title={dashboard.title}
-              color={dashboard.color}
-              createdByMe={dashboard.createdByMe}
-            />
-          </li>
-        ))}
-      </div>
-      <div className={styles.page}>
-        <Pagination
-          count={data ? data?.totalCount : 1}
-          pageSize={pageSize}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-        />
-      </div>
-    </section>
+        <div className={styles.page}>
+          <Pagination
+            count={data ? data?.totalCount : 1}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          />
+        </div>
+      </section>
+      {isOpenModal && <NewDashboardModal onClose={() => setIsOpenModal(false)} />}
+    </>
   )
 }
