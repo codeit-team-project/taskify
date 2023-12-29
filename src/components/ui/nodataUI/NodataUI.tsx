daedfaa29182cb0849bc7d5fef9ced843e2d7b1e
ParkSohyunee <sohyunee016@gmail.com>
2023-12-29 14:55:02 +0900

    Style(NodataUI): 스타일 클래스네임 수정

--- src/components/ui/nodataUI/NodataUI.tsx
+++ src/components/ui/nodataUI/NodataUI.tsx
@@ -6,15 +6,15 @@ interface NodataUIProps {
 
 export default function NodataUI({ text }: NodataUIProps) {
   return (
-    <div className={styles['no-invitation-container']}>
+    <div className={styles.container}>
       <img
-        className={styles['no-invitation-icon']}
+        className={styles.icon}
         src="/assets/icon-no-invitation.png"
         width="100px"
         height="100px"
         alt="초대받은 대시보드가 없을 때 아이콘 이미지"
       />
-      <p className={styles['no-invitation-text']}>{text}</p>
+      <p className={styles.text}>{text}</p>
     </div>
   )
 }
