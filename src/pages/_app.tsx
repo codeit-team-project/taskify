53643e9a03cc3908d6c2c35c70b11be06ed35b8f
HellenHong <hongjw030@naver.com>
2023-12-30 22:56:28 +0900

    Refactor: app 파일에서 불필요한 코드 삭제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,37 +4,10 @@ import type { AppProps } from 'next/app'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { CookiesProvider } from 'react-cookie'
-import { useRouter } from 'next/router'
-import { getCookie } from '@/utils/cookie'
-import { useCallback, useEffect } from 'react'
 
 const queryClient = new QueryClient()
 
-const DENIED_FOR_USER = ['/', '/signin', '/signup']
-
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-  const savedToken = getCookie('accessToken')
-
-  const autoMove = useCallback(() => {
-    if (savedToken) {
-      if (DENIED_FOR_USER.includes(router.pathname)) {
-        router.push('/mydashboard')
-        return
-      }
-    } else {
-      if (!DENIED_FOR_USER.includes(router.pathname)) {
-        alert('로그인이 필요합니다!')
-        router.push('/signin')
-        return
-      }
-    }
-  }, [router, savedToken])
-
-  useEffect(() => {
-    autoMove()
-  }, [autoMove])
-
   return (
     <QueryClientProvider client={queryClient}>
       <CookiesProvider>
