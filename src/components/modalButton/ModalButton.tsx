79004a7f3dbb911b79f5bcc315b88d1e73f46db3
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-12-30 20:57:51 +0900

    feat: modalButtons

---
+++ src/components/modalButton/ModalButton.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react'
+import styles from '@/components/modalButton/ModalButton.module.scss'
+
+interface ModalButtonProps {
+  children: ReactNode
+  color: string
+  size?: string
+  disabled?: boolean
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
+}
+
+export default function ModalButton({ children, color, size, ...props }: ModalButtonProps) {
+  const containerClassName = `${styles.container} ${styles[color]} ${size ? styles[size] : ''}`
+  return (
+    <button className={containerClassName} onClick={props.onClick} disabled={props.disabled}>
+      {children}
+    </button>
+  )
+}
