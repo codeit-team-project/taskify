5cef21c0947463c27fb7b41568af6e742ce8c141
ParkSohyunee <sohyunee016@gmail.com>
2023-12-30 20:22:46 +0900

    Feat(Sidebar): 사이드바 버튼 클릭시 대시보드 생성 모달 연동

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 
 import styles from './Sidebar.module.scss'
@@ -7,41 +8,51 @@ import { getDashBoardList } from '@/api/dashboards/getDashboards'
 import SidebarItem from './SidebarItem'
 
 import { DashBoardListType } from '@/types/dashBoardType'
+import NewDashboardModal from '@/components/dashboard/NewDashboardModal'
 
 export default function Sidebar() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
   const { data } = useQuery<DashBoardListType>({
     queryKey: ['dashBoards'],
     queryFn: () => getDashBoardList(),
   })
 
+  const handleOpenModal = () => {
+    setIsModalOpen(true)
+  }
+
   return (
-    <section className={styles.container}>
-      <div className={styles.logo}>
-        <Link href={'/mydashboard'}>
-          <Image src="/assets/large_logo.svg" alt="로고" width={29} height={34} priority />
-          <Image
-            src="/assets/large_Taskify.svg"
-            className={styles['logo-text']}
-            alt="로고"
-            width={80}
-            height={22}
-            priority
-          />
-        </Link>
-      </div>
-      <div className={styles['title-wrapper']}>
-        <span className={styles.title}>Dash Boards</span>
-        <button className={styles.action}>
-          <Image src="/assets/add_box.svg" alt="대시보드 추가하기 버튼" fill />
-        </button>
-      </div>
-      <div className={styles.list}>
-        {data?.dashboards.map((board) => (
-          <Link key={board.id} className={styles.menus} href={`/dashboard/${board.id}`}>
-            <SidebarItem board={board} />
+    <>
+      <section className={styles.container}>
+        <div className={styles.logo}>
+          <Link href={'/mydashboard'}>
+            <Image src="/assets/large_logo.svg" alt="로고" width={29} height={34} priority />
+            <Image
+              src="/assets/large_Taskify.svg"
+              className={styles['logo-text']}
+              alt="로고"
+              width={80}
+              height={22}
+              priority
+            />
           </Link>
-        ))}
-      </div>
-    </section>
+        </div>
+        <div className={styles['title-wrapper']}>
+          <span className={styles.title}>Dash Boards</span>
+          <button className={styles.action} onClick={handleOpenModal}>
+            <Image src="/assets/add_box.svg" alt="대시보드 추가하기 버튼" fill />
+          </button>
+        </div>
+        <div className={styles.list}>
+          {data?.dashboards.map((board) => (
+            <Link key={board.id} className={styles.menus} href={`/dashboard/${board.id}`}>
+              <SidebarItem board={board} />
+            </Link>
+          ))}
+        </div>
+      </section>
+      {isModalOpen && <NewDashboardModal onClose={() => setIsModalOpen(false)} />}
+    </>
   )
 }
