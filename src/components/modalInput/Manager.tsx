ddfccbbcad00eea7dae1412367846f99c4f3f174
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-12-31 07:22:34 +0900

    style: randomprofile이미지추가

--- src/components/modalInput/Manager.tsx
+++ src/components/modalInput/Manager.tsx
@@ -4,10 +4,11 @@ import arrowDown from '../../../public/assets/images/arrowDown.svg'
 import Image from 'next/image'
 import { Members } from '@/types/members'
 import { FormContext } from '@/context/formContext'
+import RandomProfile from '../randomProfile/RandomProfile'
 
 export type managerListType = Members[]
 
-interface ManagerProps {
+interface ManagerPropsType {
   managerId?: number
   editName?: string
   profileImageUrl?: string
@@ -19,7 +20,7 @@ export default function Manager({
   editName = '',
   profileImageUrl = '',
   managerList = [],
-}: ManagerProps) {
+}: ManagerPropsType) {
   const {
     profileImage,
     setProfileImage,
@@ -45,16 +46,17 @@ export default function Manager({
     }
   }, [])
 
+  console.log(managerList)
   return (
     <div className={styles.container}>
       <span className={styles.text}>담당자</span>
       <div onClick={handleOpenClick} className={styles.dropdownbutton}>
         <div className={styles.assigneeUser__wrapper}>
-          {profileImage && (
+          {profileImage ? (
             <div className={styles.assigneeUser__image}>
               <Image src={profileImage} alt="profile-image" width={30} height={30} />
             </div>
-          )}
+          ) : null}
 
           <span>{assigneeUserName}</span>
         </div>
@@ -73,7 +75,7 @@ export default function Manager({
                   }}
                 >
                   <div className={styles.dropdown__assigneeUser__wrapper}>
-                    {person.profileImageUrl && (
+                    {person.profileImageUrl ? (
                       <Image
                         src={person.profileImageUrl}
                         alt="profileImage"
@@ -81,7 +83,10 @@ export default function Manager({
                         width={20}
                         height={20}
                       />
+                    ) : (
+                      <RandomProfile size={20} email={person.nickname} />
                     )}
+
                     {person.nickname}
                   </div>
                 </li>
