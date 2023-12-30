4c09cf5b34d79aaec8f9c8d155a1bc86bfbf910d
HellenHong <hongjw030@naver.com>
2023-12-30 17:20:31 +0900

    Feat: 자동 페이지 이동 코드 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { CookiesProvider } from 'react-cookie'
 import { useRouter } from 'next/router'
 import { getCookie } from '@/utils/cookie'
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 
 const queryClient = new QueryClient()
 
@@ -16,13 +16,24 @@ export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
   const savedToken = getCookie('accessToken')
 
-  useEffect(() => {
+  const autoMove = useCallback(() => {
     if (savedToken) {
       if (DENIED_FOR_USER.includes(router.pathname)) {
         router.push('/mydashboard')
+        return
+      }
+    } else {
+      if (!DENIED_FOR_USER.includes(router.pathname)) {
+        alert('로그인이 필요합니다!')
+        router.push('/signin')
+        return
       }
     }
   }, [router, savedToken])
+
+  useEffect(() => {
+    autoMove()
+  }, [autoMove])
 
   return (
     <QueryClientProvider client={queryClient}>
