a520f1950dcfdbd83fa12551c89a19d432fd395e
HellenHong <hongjw030@naver.com>
2024-01-01 11:20:14 +0900

    Feat: 미들웨어 코드 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,16 @@
-import { getCookie } from '@/utils/cookie'
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
-export async function middleware(request: NextRequest) {
-  // 요청 헤더에서 로그인 여부를 확인할 수 있도록 쿠키에 접근
-  const jwt = getCookie('accessToken')
+const REQUIRED_AUTH = ['/my', '/mydashboard', '/dashboard', '/dashboard/:path*']
+const NOT_REQUIRED_AUTH = ['/', '/signin', '/signup']
 
-  // 로그인 상태가 아니면 Redirection
-  if (!jwt) {
+export async function middleware(request: NextRequest) {
+  const jwt = request.cookies.get('accessToken')
+  if (!jwt && REQUIRED_AUTH.includes(request.nextUrl.pathname)) {
     return NextResponse.redirect(new URL(`/signin`, request.url))
   }
-
-  // 로그인 상태면 원래 요청한 경로로 이동한다.
+  if (jwt && NOT_REQUIRED_AUTH.includes(request.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL(`/mydashboard`, request.url))
+  }
   return NextResponse.next()
-}
-
-//
-export const config = {
-  matcher: ['/my', '/mydashboard', '/dashboard/:path*', '/dashboard'],
 }
