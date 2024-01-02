e5c6cfb8967a0e40435458b34866821335343ff6
HellenHong <hongjw030@naver.com>
2024-01-02 18:12:25 +0900

    Feat: error page 생성

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/router'
+import styles from './index.module.scss'
+import { getCookie } from '@/utils/cookie'
+
+export default function NotFound() {
+  const router = useRouter()
+  const savedToken = getCookie('accessToken')
+
+  const handleRouting = () => {
+    if (savedToken) router.push('/mydashboard')
+    else router.push('/')
+  }
+
+  return (
+    <div className={styles.background}>
+      <div className={styles.container}>
+        <img src="/assets/error.png" alt="error img" />
+      </div>
+      <div className={styles.line}></div>
+      <button onClick={handleRouting}> Home </button>
+    </div>
+  )
+}
