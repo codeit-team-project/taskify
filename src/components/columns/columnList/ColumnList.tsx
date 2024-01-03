a5cf5fcbb51ba58bbc15f8264e69689bc1182097
HellenHong <hongjw030@naver.com>
2024-01-03 13:37:57 +0900

    Refactor: isLoading 추가

--- src/components/columns/columnList/ColumnList.tsx
+++ src/components/columns/columnList/ColumnList.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import styles from './ColumnList.module.scss'
 import AddColumnButton from '@/components/columns/addColumnButton/AddColumnButton'
 import ColumnItem from '../columnItem/ColumnItem'
@@ -9,8 +10,23 @@ interface ColumnListProps {
   boardId: number
 }
 
+function NotFoundBoard() {
+  return (
+    <div className={styles['no-invitation-container']}>
+      <Image
+        className={styles['no-invitation-icon']}
+        src="/assets/icon-no-invitation.png"
+        width={100}
+        height={100}
+        alt="대시보드가 없을 때 아이콘 이미지"
+      />
+      <p className={styles['no-invitation-text']}>대시보드가 존재하지 않아요.</p>
+    </div>
+  )
+}
+
 export default function ColumnList({ boardId }: ColumnListProps) {
-  const { data } = useQuery<ColumnsType>({
+  const { data, isLoading } = useQuery<ColumnsType>({
     queryKey: ['getColumns', boardId],
     queryFn: () => getColumns(boardId),
     enabled: !!boardId,
@@ -19,12 +35,18 @@ export default function ColumnList({ boardId }: ColumnListProps) {
 
   return (
     <>
-      <ul className={styles['column-list']}>
-        {data?.data.map((item) => {
-          return <ColumnItem key={item.id} item={item} />
-        })}
-        <AddColumnButton dashBoardId={boardId} />
-      </ul>
+      {!data && !isLoading ? (
+        <NotFoundBoard />
+      ) : data && !isLoading ? (
+        <ul className={styles['column-list']}>
+          {data?.data.map((item) => {
+            return <ColumnItem key={item.id} item={item} />
+          })}
+          <AddColumnButton dashBoardId={boardId} />
+        </ul>
+      ) : (
+        <></>
+      )}
     </>
   )
 }
