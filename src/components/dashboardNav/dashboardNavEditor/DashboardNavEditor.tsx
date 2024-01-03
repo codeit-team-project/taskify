8ffa1cd5e0a1d87f997fa6c1a1b477ee448ff80b
ParkSohyunee <sohyunee016@gmail.com>
2024-01-03 18:29:26 +0900

    Refactor(DashboardNavEditor): 프로필 여부 로직 개선 및 uuid 제거

--- src/components/dashboardNav/dashboardNavEditor/DashboardNavEditor.tsx
+++ src/components/dashboardNav/dashboardNavEditor/DashboardNavEditor.tsx
@@ -10,7 +10,6 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { createPortal } from 'react-dom'
 import { useQuery } from '@tanstack/react-query'
-import { v4 as uuidv4 } from 'uuid'
 
 import { getDashBoardMembers } from '@/api/members/getMembers'
 import InvitationModal from '@/components/invitations/InvitationModal'
@@ -91,31 +90,24 @@ export default function DashboardNavEditor({ isOwner = false, boardId }: Dashboa
           <div className={styles['members-section']}>
             {windowSize > 744 ? (
               <div
-                key={uuidv4()}
                 className={styles['member-img-list']}
                 style={{
                   width: `${memberData.totalCount >= 5 ? 17 : memberData.totalCount * 3 + 2}rem`,
                 }}
               >
                 {memberData.members.slice(0, 4).map((member) => {
-                  if (member) {
-                    return (
-                      <>
-                        {member.profileImageUrl ? (
-                          <Image
-                            key={member.id}
-                            src={member.profileImageUrl}
-                            alt="profile"
-                            width={36}
-                            height={36}
-                            className={styles['member-img']}
-                          />
-                        ) : (
-                          <RandomProfile size={36} key={member.id} email={member.email} />
-                        )}
-                      </>
-                    )
-                  }
+                  return member.profileImageUrl ? (
+                    <Image
+                      key={member.id}
+                      src={member.profileImageUrl}
+                      alt="profile"
+                      width={36}
+                      height={36}
+                      className={styles['member-img']}
+                    />
+                  ) : (
+                    <RandomProfile size={36} key={member.id} email={member.email} />
+                  )
                 })}
                 {memberData.totalCount > 4 && (
                   <div className={styles['rest-member-img']}>+{memberData.totalCount - 4}</div>
@@ -129,24 +121,18 @@ export default function DashboardNavEditor({ isOwner = false, boardId }: Dashboa
                 }}
               >
                 {memberData.members.slice(0, 2).map((member) => {
-                  if (member) {
-                    return (
-                      <>
-                        {member.profileImageUrl ? (
-                          <Image
-                            key={member.id}
-                            src={member.profileImageUrl}
-                            alt="profile"
-                            width={36}
-                            height={36}
-                            className={styles['member-img']}
-                          />
-                        ) : (
-                          <RandomProfile size={36} key={member.id} email={member.email} />
-                        )}
-                      </>
-                    )
-                  }
+                  return member.profileImageUrl ? (
+                    <Image
+                      key={member.id}
+                      src={member.profileImageUrl}
+                      alt="profile"
+                      width={36}
+                      height={36}
+                      className={styles['member-img']}
+                    />
+                  ) : (
+                    <RandomProfile size={36} key={member.id} email={member.email} />
+                  )
                 })}
                 {memberData.totalCount > 2 && (
                   <div className={styles['rest-member-img']}>+{memberData.totalCount - 2}</div>
