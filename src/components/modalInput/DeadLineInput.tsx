12f980d8049d7d6f95704c604f61179f31767026
yunajoe <86518113+yunajoe@users.noreply.github.com>
2024-01-04 15:08:57 +0900

    deadlineInput에서 날짜외에 못 들어가게함

--- src/components/modalInput/DeadLineInput.tsx
+++ src/components/modalInput/DeadLineInput.tsx
@@ -4,7 +4,13 @@ import styles from '@/components/modalInput/DeadLineInput.module.scss'
 const DeadLineInput = forwardRef((props, ref: React.LegacyRef<HTMLInputElement>) => {
   return (
     <label>
-      <input {...props} ref={ref} className={styles.container} placeholder="날짜를 입력해주세요" />
+      <input
+        {...props}
+        ref={ref}
+        className={styles.container}
+        placeholder="날짜를 입력해주세요"
+        readOnly
+      />
     </label>
   )
 })
